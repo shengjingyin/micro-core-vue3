e526c793558394e1f142fb903a80c159becdab32
阿银 <739178270@qq.com>
2022-09-17 20:55:07 +0800

    stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,14 +1,38 @@
+import { extend } from "../shared/index";
 class ReactiveEffect {
     private _fn: any;
-    constructor(fn, public scheduler?) {
+    deps = [];
+    active = true;
+    scheduler: any;
+    onStop: any;
+    constructor(fn, options) {
+        // const { scheduler, onStop } = options;
+        // this.scheduler = scheduler;
+        // this.onStop = onStop;
+        extend(this, options);
         this._fn = fn;
     }
     run() {
         activeEffect = this;
         return this._fn();
     }
+    stop() {
+        if (this.active) {
+            cleanupEffect(this);
+            if (this.onStop) {
+                this.onStop();
+            }
+            this.active = false;
+        }
+    }
+}
+function cleanupEffect(effect: ReactiveEffect) {
+    effect.deps.forEach((dep: Set<ReactiveEffect>) => {
+        dep.delete(effect);
+    });
 }
 const targetMap = new Map();
+// 记录
 export function track(target, key) {
     // 一个实例多个target对象，一个target对象多个属性，每一个属性是只有一个dep, 存储响应函数
     // target（map） - key（map） - dep（set）;
@@ -23,9 +47,12 @@ export function track(target, key) {
         dep = new Set();
         depsMap.set(key, dep);
     }
-    dep.add(activeEffect);
+    if (!activeEffect) return;
+    // 双向绑定
+    dep.add(activeEffect); // dep 关联多少个 响应函数
+    activeEffect.deps.push(dep); // 关联函数 对应多少个 dep
 }
-
+// 更新
 export function trigger(target, key) {
     const depsMap = targetMap.get(target);
     const dep = depsMap.get(key);
@@ -40,10 +67,16 @@ export function trigger(target, key) {
 let activeEffect;
 export function effect(fn, options: any = {}) {
     // fn ReactiveEffect
-    const { scheduler } = options;
-    const _effect = new ReactiveEffect(fn, scheduler);
+    const _effect = new ReactiveEffect(fn, options);
 
     _effect.run();
 
-    return _effect.run.bind(_effect);
+    const runner: any = _effect.run.bind(_effect);
+
+    runner.effect = _effect;
+    return runner;
+}
+
+export function stop(runner) {
+    runner.effect.stop();
 }
